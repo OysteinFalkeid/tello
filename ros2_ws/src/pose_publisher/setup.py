b40bb7699dc00b3fade966685caec11817eaa770
WilliamFBD <william.fr.dahl@gmail.com>
2025-11-27 12:01:10 +0100

    Remade python package and added entry point

---
+++ ros2_ws/src/pose_publisher/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'pose_publisher'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='william',
+    maintainer_email='william.fr.dahl@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'pose_publisher = pose_publisher.pose_publisher_node:main',
+        ],
+    },
+)
