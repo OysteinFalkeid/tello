85c7c0803b33783f70c8c280e655245e4776cd7d
Hakon Bråten <your_github_email_here>
2025-12-09 15:38:38 +0100

    final

--- quaternion_converter.py
+++
@@ -1,9 +0,0 @@
-import math
-yaw_radians = 0.0
-yaw_deg = 180.0
-if yaw_deg:
-    yaw_radians = yaw_deg / 180.0 * math.pi
-print(f"x: 0.0")
-print(f"y: 0.0")
-print(f"z: {math.sin(yaw_radians)}")
-print(f"w: {math.cos(yaw_radians/2)}")
